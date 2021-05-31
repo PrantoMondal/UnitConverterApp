7099af93cb2850be34e6ae0bdabfeecda9cf5b7b
PrantoMondal <kumar.pranto@northsouth.edu>
2021-06-01 00:00:07 +0600

    modified

--- app/src/main/java/com/example/unitconverterapp/MainActivity.java
+++ app/src/main/java/com/example/unitconverterapp/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private TextView textView;
+    private EditText editText;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
     }
 }
