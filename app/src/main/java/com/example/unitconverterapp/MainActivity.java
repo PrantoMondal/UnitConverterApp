9e32a293729f33f4e24067dfc976881e2e99f750
PrantoMondal <kumar.pranto@northsouth.edu>
2021-06-01 00:34:43 +0600

    Toast added

--- app/src/main/java/com/example/unitconverterapp/MainActivity.java
+++ app/src/main/java/com/example/unitconverterapp/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.unitconverterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
@@ -19,5 +21,13 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
         editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hi click Listener worked!", Toast.LENGTH_SHORT).show();
+                String s = editText.getText().toString();
+                int a = Integer.parseInt(s);
+            }
+        });
     }
 }
